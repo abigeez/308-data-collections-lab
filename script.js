c9babaa3efa51bbbeec630ba84d7dbc594e0ce47
Abi Garner <abigail.gee@hotmail.com>
2024-04-19 19:32:41 -0400

    adding some modifcations to part one and two

--- script.js
+++ script.js
@@ -7,28 +7,33 @@ let rows =[]
 //array to set up the initial columns
 let cells =[]
 // taking the string and splitting it  by the /n to create a new substring for each break
-rows=csvStr.split("/n");
+rows=csvStr.split("\n");
+console.log(rows)
 // iterating through every line and executing cells to be split by the comma 
 for(let i=0;i<rows.length;i++){
-    cells = rows[i].split(",");
+    cells = rows[i].split("\n");
     console.log(`${cells[0]} ${cells[1]} ${cells[2]} ${cells[3]}`)
 }
 // console logging the cells by their iteration which produces the value of the first four columns
 
 ////part 2 expanding functionality
 let csvStr1 = "ID,Name,Occupation,Age,\n42,Bruce,Knight,41,\n57,Bob,Fry Cook,19,\n63,Blaine,Quiz Master,58,\n98,Bill,Doctor's Assistant,26"
-let numCol = [];
+let numCol = (`${cells[0]} ${cells[1]} ${cells[2]} ${cells[3]}`);
 innerArray=[];
 
 //variable that stores the number of columns
 
 cells=rows[0].split(",");
 //declaring the amt of columns that are in the first row.
+console.log(cells)
+
 
 numCol=cells.length;
 console.log(`There are: ${numCol}`) ;
-//logging the amount of columns there are 
+//logging the amount of columns there are.
 
+
+// a loop that works with n amount of columns 
 for(i=0;i<rows.length;i++) {
     cells = rows[i].split(",");
     if(cells.length>numCol){
@@ -38,3 +43,9 @@ for(i=0;i<rows.length;i++) {
 }
 console.log(`2d array`)
 console.log(innerArray)
+
+
+
+
+//part3 transforming data 
+
